3d40952f709ae8738b9d9a84a2efab824e0d4030
Vipin Yadav <ivipinyadav14@gmail.com>
2024-05-24 02:07:31 +0530

    add contact form functionality

--- src/script.js
+++ src/script.js
@@ -1,3 +1,17 @@
-function handleMenu() {
-    console.log("Menu Clicked");
+function sendMsg() {
+    let fullName = document.getElementById("name").value;
+    let msg = document.getElementById("msg").value;
+    let email = document.getElementById("email").value;
+
+    let body = "Name : " + fullName + "<br> Email : " + email + "<br> Message : " + msg;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "vipinyadav.work@gmail.com",
+        Password : "4CA07B939F4C15C08913D6C1CFA082E3FA0E",
+        To : 'vipinyadav.work@gmail.com',
+        From : "vipinyadav.work@gmail.com",
+        Subject : "Message sent via My Portfolio Website",
+        Body : body
+    });
 }
