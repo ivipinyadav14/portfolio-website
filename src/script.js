f8f71641d1abf5c5d9b05a0fca8c66f6fa82b843
Vipin Yadav <ivipinyadav14@gmail.com>
2025-06-11 03:25:00 +0530

    fix-form

--- src/script.js
+++ src/script.js
@@ -1,17 +1,40 @@
-function sendMsg() {
-    let fullName = document.getElementById("name").value;
-    let msg = document.getElementById("msg").value;
-    let email = document.getElementById("email").value;
+const form = document.getElementById('form');
+const result = document.getElementById('result');
 
-    let body = "Name : " + fullName + "<br> Email : " + email + "<br> Message : " + msg;
+form.addEventListener('submit', function(e) {
+    const formData = new FormData(form);
+    e.preventDefault();
 
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "vipinyadav.work@gmail.com",
-        Password : "4CA07B939F4C15C08913D6C1CFA082E3FA0E",
-        To : 'vipinyadav.work@gmail.com',
-        From : "vipinyadav.work@gmail.com",
-        Subject : "Message sent via My Portfolio Website",
-        Body : body
-    });
-}
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
